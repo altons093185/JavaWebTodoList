2fac6195f54453e10fb4b8cead3af059ffb20c0b
altons093185 <altons093185@gmail.com>
2025-04-25 12:34:51 +0800

    0425

--- src/main/java/controller/TodoListServlet.java
+++ src/main/java/controller/TodoListServlet.java
@@ -1,41 +1,74 @@
 package controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class TodoListServlet
  */
-@WebServlet("/TodoListServlet")
+@WebServlet("/todolist/*")
 public class TodoListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TodoListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public TodoListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String pathInfo = req.getPathInfo();
+		resp.getWriter().print("pathInfo = " + pathInfo);
+		switch (pathInfo) {
+		case "/": // 顯示 Todo List 首頁
+
+			break;
+		case "/update": // 修改 Todo 紀錄
+			String id = req.getParameter("id");
+			String completed = req.getParameter("checked");
+			String text = req.getParameter("text");
+
+			if (completed != null) {
+				// 修改 Todo 完成狀態
+
+			} else if (text != null) {
+				// 修改 Todo 內容
+
+			}
+
+			break;
+		case "/delete": // 刪除某一項 Todo 紀錄
+
+			break;
+		default: // 錯誤路徑
+			resp.getWriter().print("path error");
+			return;
+		}
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String pathInfo = req.getPathInfo();
+		if (!pathInfo.equals("/add")) {
+			// 錯誤路徑
+			resp.getWriter().print("path error");
+			return;
+		}
+		// 進行新增程序
+
+	}
 }
