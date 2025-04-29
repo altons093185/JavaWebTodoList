47938b0ecb60e2056ba588ef08f504cb7f99357e
altons093185 <altons093185@gmail.com>
2025-04-29 16:25:32 +0800

    0429

--- src/main/java/controller/TodoListServlet.java
+++ src/main/java/controller/TodoListServlet.java
@@ -1,19 +1,24 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dto.TodoDTO;
+import service.TodoListService;
+import service.TodoListServiceImpl;
 
 /**
  * Servlet implementation class TodoListServlet
  */
 @WebServlet("/todolist/*")
 public class TodoListServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private TodoListService service = new TodoListServiceImpl();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -32,8 +37,12 @@ public class TodoListServlet extends HttpServlet {
 		resp.getWriter().print("pathInfo = " + pathInfo);
 		switch (pathInfo) {
 		case "/": // 顯示 Todo List 首頁
+			List<TodoDTO> todos = service.findAllTodos();
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/todolist.jsp");
+			req.setAttribute("todos", todos);
+			rd.forward(req, resp);
+			return;
 
-			break;
 		case "/update": // 修改 Todo 紀錄
 			String id = req.getParameter("id");
 			String completed = req.getParameter("checked");
